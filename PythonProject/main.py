27685cd0cb78a2b7d932ec08a1a7cf7504520560
Nika Vardidze <nika.vardidze26@gmail.com>
2026-05-22 12:09:07 +0200

    Fixed formatting and paths

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,8 +1,11 @@
 def multiply(a, b):
     return a * b
 
+
 def main():
     print(multiply(2, 3))
 
+
 if __name__ == "__main__":
     main()
+    
--- PythonProject/test_main.py
+++ PythonProject/test_main.py
@@ -1,5 +1,6 @@
 from main import multiply
 
+
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(4, 5) == 20
